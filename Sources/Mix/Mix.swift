e111d13924b3900ee9bb1e948aace458a54c189a
Sylvan Martin <alephnulltim@gmail.com>
2023-04-11 15:33:46 -0400

    Initial Commit

---
+++ Sources/Mix/Mix.swift
@@ -0,0 +1,6 @@
+public struct Mix {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MixTests/MixTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Mix
+
+final class MixTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Mix().text, "Hello, World!")
+    }
+}
